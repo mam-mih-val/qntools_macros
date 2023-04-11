a7e4f2596e4e5eb9c8ec666b01ed558cab41ebbb
Mikhail <mam.mih.val@gmail.com>
2023-04-11 17:14:26 +0300

    - Cleanup

--- src/env.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ogolosov/desktop/analysis/qntools/install/lib
-export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:/home/ogolosov/desktop/analysis/qntools/install/include/QnTools
