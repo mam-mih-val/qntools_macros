5643156aed4a01c6c8d9ec845b40755c309d8cce
Mikhail <mam.mih.val@gmail.com>
2023-04-17 15:03:47 +0300

    More verbose batch script

--- batch/run_slurm.sh
+++ batch/run_slurm.sh
@@ -20,7 +20,7 @@ cd $id
 source /mnt/pool/nica/7/mam2mih/soft/basov/root-6.24.06/install/bin/thisroot.sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/pool/nica/7/mam2mih/soft/basov/QnTools/install/lib
 
-echo "/mnt/pool/nica/7/mam2mih/soft/basov/bmn_particle_finder/build/find_candidates /mnt/pool/nica/7/mam2mih/soft/basov/bmn_particle_finder/macro/lambda.cc $list_dir/$file_list"
+echo "/mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correct /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/example.cc $list_dir/$file_list"
 
 # PLAIN
 time /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correct /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/example.cc $list_dir/$file_list
@@ -31,6 +31,7 @@ mv correction_out.root correction_in.root
 # TWIST AND RESCALING
 time /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correct /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/example.cc $list_dir/$file_list
 
+echo "/mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correlate /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/correlate.cc correction_out.root"
 time /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correlate /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/correlate.cc correction_out.root
 
 echo "The End."
