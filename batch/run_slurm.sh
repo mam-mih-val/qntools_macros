2248e6f30e6bf2987271c8f43774fe9e993d34f1
Mikhail <mam.mih.val@gmail.com>
2023-04-17 14:52:41 +0300

    Script for basov's slurm is added

---
+++ batch/run_slurm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH -p fast
+#SBATCH -t 01:00:00
+#SBATCH -J finder
+#SBATCH -o /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/log/%A_%a.log
+
+list_dir=${1}
+output_dir=${2}
+
+id=$SLURM_ARRAY_TASK_ID
+
+file_list=$( ls $list_dir | head -n $id | tail -n 1 )
+
+mkdir -p $output_dir
+cd $output_dir
+mkdir $id
+cd $id
+
+source /mnt/pool/nica/7/mam2mih/soft/basov/root-6.24.06/install/bin/thisroot.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/pool/nica/7/mam2mih/soft/basov/QnTools/install/lib
+
+echo "/mnt/pool/nica/7/mam2mih/soft/basov/bmn_particle_finder/build/find_candidates /mnt/pool/nica/7/mam2mih/soft/basov/bmn_particle_finder/macro/lambda.cc $list_dir/$file_list"
+
+# PLAIN
+time /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correct /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/example.cc $list_dir/$file_list
+mv correction_out.root correction_in.root
+# RECENTERING
+time /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correct /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/example.cc $list_dir/$file_list
+mv correction_out.root correction_in.root
+# TWIST AND RESCALING
+time /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correct /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/example.cc $list_dir/$file_list
+
+time /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/build/correlate /mnt/pool/nica/7/mam2mih/soft/basov/qntools_macros/macro/correlate.cc correction_out.root
+
+echo "The End."
