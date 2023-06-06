0379c0eba5a92bbaa13a4b88fe6471156ce60a8a
Mikhail <mam.mih.val@gmail.com>
2023-06-06 23:13:14 +0300

    Arbitrary number of arguments can be passed to a macro

--- src/correct.cc
+++ src/correct.cc
@@ -10,9 +10,11 @@ int main(int n_args, char** args){
   if( n_args < 2 )
     throw std::runtime_error( "No argumets provided" );
   std::string macro{args[1]};
-  std::string list{args[2]};
-  std::string efficiency_file{args[3]};
-  std::string macro_full = macro+"(\""+list+"\",\""+efficiency_file+"\")";
+  std::string macro_full = macro+"(";
+  for( int i=2; i<n_args; ++i ){
+    macro_full+="\""+ std::string{args[i]}+"\",";
+  }
+  macro_full.back()=')';
   std::cout << "Executing "+macro_full << std::endl;
   gROOT->Macro(macro_full.c_str());
   return 0;
