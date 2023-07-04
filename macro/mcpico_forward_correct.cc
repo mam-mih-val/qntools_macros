c7501e764729746e39c923a7e09125890af0a428
Mikhail <mam.mih.val@gmail.com>
2023-07-04 19:19:41 +0300

    Setup to check only a forward vectors correlations

---
+++ macro/mcpico_forward_correct.cc
@@ -0,0 +1,119 @@
+
+//
+// Created by Misha on 3/7/2023.
+//
+
+void mcpico_correct(std::string list){
+  const float Y_BEAM = 0.731032;
+  TStopwatch timer;
+  timer.Start();
+  std::string treename = "mctree";
+  TFileCollection collection( "collection", "", list.c_str() );
+  auto* chain = new TChain( treename.c_str() );
+  chain->AddFileInfoList( collection.GetList() );
+  ROOT::RDataFrame d( *chain );
+  auto dd=d
+          .Define( "reaction_plane", [](){
+            std::random_device rnd_device;
+            std::mt19937 generator(rnd_device());
+            std::uniform_real_distribution<float> distribution(-M_PI,M_PI); // distribution in range [0, 1]
+            return distribution(generator);
+          }, {} )
+          .Define("phi", []( ROOT::VecOps::RVec<float> vec_px, ROOT::VecOps::RVec<float> vec_py, float psi_rp ){
+            ROOT::VecOps::RVec<float> vec_phi;
+            for( int i=0; i<vec_px.size(); ++i ){
+              auto px = vec_px.at(i);
+              auto py = vec_py.at(i);
+              auto phi = atan2(py, px);
+              phi+=psi_rp;
+              vec_phi.push_back( phi );
+            }
+            return vec_phi;
+          },{ "momx", "momy", "reaction_plane" })
+          .Define("pT", []( ROOT::VecOps::RVec<float> vec_px, ROOT::VecOps::RVec<float> vec_py ){
+            ROOT::VecOps::RVec<float> vec_pT;
+            for( int i=0; i<vec_px.size(); ++i ){
+              auto px = vec_px.at(i);
+              auto py = vec_py.at(i);
+              vec_pT.push_back( sqrt(px*px + py*py) );
+            }
+            return vec_pT;
+          },{ "momx", "momy", })
+          .Define("y", []( ROOT::VecOps::RVec<float> vec_pz, ROOT::VecOps::RVec<float> vec_E ){
+            ROOT::VecOps::RVec<float> vec_y;
+            for( int i=0; i<vec_pz.size(); ++i ){
+              auto pz = vec_pz.at(i);
+              auto E = vec_E.at(i);
+              auto y = 0.5 * log( (E+pz)/(E-pz) );
+              vec_y.push_back( y );
+            }
+            return vec_y;
+          },{ "momz", "ene", })
+          .Define("eta_lab",[Y_BEAM]( ROOT::VecOps::RVec<float> vec_y, ROOT::VecOps::RVec<float> vec_pT, ROOT::VecOps::RVec<int> vec_pid ){
+            ROOT::VecOps::RVec<float> vec_eta;
+            for( int i=0; i<vec_y.size(); ++i ){
+              auto y = vec_y.at(i);
+              auto pT = vec_pT.at(i);
+              auto pid = vec_pid.at(i);
+              auto mass = TDatabasePDG::Instance()->GetParticle(pid)->Mass();
+              auto mT = sqrt( pT*pT + mass*mass );
+              auto eta_lab = asinh( mT/pT * sinh( y + Y_BEAM ) );
+              vec_eta.push_back(eta_lab);
+            }
+            return vec_eta;
+          },{ "y", "pT", "pdg", })
+          .Filter("bimp<14.0")
+          ;
+
+  auto correction_task = CorrectionTask( dd, "correction_out.root", "qa.root" );
+  correction_task.SetEventVariables( std::regex("bimp|reaction_plane") );
+  correction_task.SetTrackVariables({
+    std::regex("phi|pT|y|eta_lab|pdg|rnd_sub|charge")
+  });
+
+  correction_task.InitVariables();
+  correction_task.AddEventAxis( {"bimp", 14, 0, 14} );
+
+  VectorConfig f1( "F1", "phi", "Ones", VECTOR_TYPE::TRACK, NORMALIZATION::M );
+  f1.SetHarmonicArray( {1, 2} );
+  f1.SetCorrections( {CORRECTION::PLAIN } );
+//  f1.AddCut( "pdg", [](double pid){
+//    auto pdg_code = static_cast<int>(pid);
+//    return pdg_code == 2112;
+//  }, "proton cut" );
+  f1.AddCut( "eta_lab", [](double eta){
+    return 3.8 < eta && eta < 5.4;
+    }, "F1 Cut" );
+  correction_task.AddVector(f1);
+
+  VectorConfig f2( "F2", "phi", "Ones", VECTOR_TYPE::TRACK, NORMALIZATION::M );
+  f2.SetHarmonicArray( {1, 2} );
+  f2.SetCorrections( {CORRECTION::PLAIN } );
+//  f2.AddCut( "pdg", [](double pid){
+//    auto pdg_code = static_cast<int>(pid);
+//    return pdg_code == 2112;
+//  }, "proton cut" );
+  f2.AddCut( "eta_lab", [](double eta){
+    return 3.3 < eta && eta < 3.8;
+    }, "F2 Cut" );
+  correction_task.AddVector(f2);
+
+  VectorConfig f3( "F3", "phi", "Ones", VECTOR_TYPE::TRACK, NORMALIZATION::M );
+  f3.SetHarmonicArray( {1, 2} );
+  f3.SetCorrections( {CORRECTION::PLAIN } );
+//  f3.AddCut( "pdg", [](double pid){
+//    auto pdg_code = static_cast<int>(pid);
+//    return pdg_code == 2112;
+//  }, "proton cut" );
+  f3.AddCut( "eta_lab", [](double eta){
+    return 2.7 < eta && eta < 3.3;
+    }, "F3 Cut" );
+  correction_task.AddVector(f3);
+
+  VectorConfig psi_rp( "psi_rp", "reaction_plane", "Ones", VECTOR_TYPE::TRACK, NORMALIZATION::M );
+  psi_rp.SetHarmonicArray( {1, 2} );
+  psi_rp.SetCorrections( {CORRECTION::PLAIN } );
+  correction_task.AddVector(psi_rp);
+
+  correction_task.Run();
+}
---
+++ macro/mcpico_forward_correlate.cc
@@ -0,0 +1,64 @@
+#include "QnDataFrame.hpp"
+
+vector <vector<string>> Q1Q1=
+{
+  {"F1_PLAIN", "F2_PLAIN"},
+  {"F1_PLAIN", "F3_PLAIN"},
+  {"F2_PLAIN", "F3_PLAIN"},
+};
+
+vector <vector<string>> Q1Psi1=
+{
+  {"F1_PLAIN", "psi_rp_PLAIN"},
+  {"F2_PLAIN", "psi_rp_PLAIN"},
+  {"F3_PLAIN", "psi_rp_PLAIN"},
+};
+
+void mcpico_correlate(string inputFiles="qn.root", string outputFile="corr.root")
+{
+  int nSamples = 100;
+  Qn::AxisD centAxis({"bimp", 14, 0, 14});
+  auto axes_correlation = Qn::MakeAxes(centAxis);
+  ROOT::RDataFrame d( "tree", inputFiles.c_str() );
+  auto d_samples = Qn::Correlation::Resample(d, nSamples);
+
+  namespace P2 = Qn::Correlation::TwoParticle;
+  namespace P3 = Qn::Correlation::MixedHarmonics;
+  auto wn = Qn::Correlation::UseWeights::No;
+  auto wy = Qn::Correlation::UseWeights::Yes;
+  auto wUnity = [](const Qn::QVector &a, const Qn::QVector &b) { return 1; };
+  auto wSumWu = [](const Qn::QVector &a, const Qn::QVector &b) { return a.sumweights(); };
+  auto wSumWu3part = [](const Qn::QVector &a, const Qn::QVector &b, const Qn::QVector &c) { return a.sumweights(); };
+
+  auto corrBuilder =
+    Qn::Correlation::CorrelationBuilder{&d_samples, nSamples, axes_correlation};
+  for (auto &corr:Q1Q1)
+  {
+    std::array<std::string, 2> qn{corr.at(0), corr.at(1)};
+    string corrName=corr.at(0)+"."+corr.at(1);
+    corrBuilder.AddCorrelationWithInternalReader(corrName+".x1x1", P2::xx(1, 1), wUnity, wn, qn, qn);
+    corrBuilder.AddCorrelationWithInternalReader(corrName+".y1y1", P2::yy(1, 1), wUnity, wn, qn, qn);
+    corrBuilder.AddCorrelationWithInternalReader(corrName+".x1y1", P2::xy(1, 1), wUnity, wn, qn, qn);
+    corrBuilder.AddCorrelationWithInternalReader(corrName+".y1x1", P2::yx(1, 1), wUnity, wn, qn, qn);
+  }
+  for (auto &corr:Q1Psi1)
+  {
+    std::array<std::string, 2> qn{corr.at(0), corr.at(1)};
+    string corrName=corr.at(0)+"."+corr.at(1);
+    corrBuilder.AddCorrelationWithInternalReader(corrName+".x1x1", P2::xx(1, 1), wUnity, wn, qn, qn);
+    corrBuilder.AddCorrelationWithInternalReader(corrName+".y1y1", P2::yy(1, 1), wUnity, wn, qn, qn);
+    corrBuilder.AddCorrelationWithInternalReader(corrName+".x1y1", P2::xy(1, 1), wUnity, wn, qn, qn);
+    corrBuilder.AddCorrelationWithInternalReader(corrName+".y1x1", P2::yx(1, 1), wUnity, wn, qn, qn);
+  }
+
+  // ---------------- //
+  // saving to output //
+  // ---------------- //
+  auto corrFile = TFile::Open(outputFile.c_str(), "RECREATE");
+  corrFile->cd();
+  auto results = corrBuilder.GetResults();
+  for (auto &res : results) {
+    res->Write();
+  }
+  corrFile->Close();
+}
