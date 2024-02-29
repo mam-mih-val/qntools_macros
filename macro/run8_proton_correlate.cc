cf64c0ee0ce82239bdcecb2d2f115284a0953525
Mikhail <mam.mih.val@gmail.com>
2024-02-29 19:47:04 +0300

    Fix.

--- macro/run8_proton_correlate.cc
+++ macro/run8_proton_correlate.cc
@@ -103,13 +103,13 @@ vector <vector<string>> u1_rescaled_Q1_rescaled=
   {"proton_RESCALED", "F2_RESCALED"},
   {"proton_RESCALED", "F3_RESCALED"},
 
-  {"proton_nhits8", "F1_RESCALED"},
-  {"proton_nhits8", "F2_RESCALED"},
-  {"proton_nhits8", "F3_RESCALED"},
+  {"proton_nhits8_RESCALED", "F1_RESCALED"},
+  {"proton_nhits8_RESCALED", "F2_RESCALED"},
+  {"proton_nhits8_RESCALED", "F3_RESCALED"},
 
-  {"proton_nhits9", "F1_RESCALED"},
-  {"proton_nhits9", "F2_RESCALED"},
-  {"proton_nhits9", "F3_RESCALED"},
+  {"proton_nhits9_RESCALED", "F1_RESCALED"},
+  {"proton_nhits9_RESCALED", "F2_RESCALED"},
+  {"proton_nhits9_RESCALED", "F3_RESCALED"},
 };
 
 vector <vector<string>> Q1_rescaled_Q1_rescaled =
