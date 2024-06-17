edc9b1c922982eca24cd8db61b456a1f1906e5d3
Mikhail <mam.mih.val@gmail.com>
2024-06-17 23:04:44 +0300

    Fix

--- macro/run8_proton_correlate_runid.cc
+++ macro/run8_proton_correlate_runid.cc
@@ -102,7 +102,7 @@ namespace P4{
   }
 
 }
-void run8_proton_correlate_vtx(string inputFiles="qn.root", string outputFile="corr.root")
+void run8_proton_correlate_runid(string inputFiles="qn.root", string outputFile="corr.root")
 {
   int nSamples = 100;
   Qn::AxisD centAxis({"centrality", 4, 0, 40});
