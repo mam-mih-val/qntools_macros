36b7c11194e1152d552bd1268d0eb7ba6fdaaadc
Mikhail Mamaev <mam.mih.val@gmail.com>
2024-10-11 14:54:15 +0300

    Additional correlations for v2 and v3

--- macro/run8_proton_correlate_clean.cc
+++ macro/run8_proton_correlate_clean.cc
@@ -24,6 +24,10 @@ vector <vector<string>> Q1_rescaled_Q1_rescaled =
 
 vector <vector<string>> u2Q1Q1_rescaled=
 { 
+  {"proton_RESCALED", "F1_RESCALED", "F1_RESCALED"},
+  {"proton_RESCALED", "F2_RESCALED", "F2_RESCALED"},
+  {"proton_RESCALED", "F3_RESCALED", "F3_RESCALED"},
+
   {"proton_RESCALED", "F1_RESCALED", "F2_RESCALED"},
   {"proton_RESCALED", "F2_RESCALED", "F3_RESCALED"},
   {"proton_RESCALED", "F1_RESCALED", "F3_RESCALED"},
@@ -31,6 +35,10 @@ vector <vector<string>> u2Q1Q1_rescaled=
 
 vector <vector<string>> u3Q1Q1Q1_rescaled=
 {
+  {"proton_RESCALED", "F1_RESCALED", "F1_RESCALED", "F1_RESCALED"},
+  {"proton_RESCALED", "F2_RESCALED", "F2_RESCALED", "F2_RESCALED"},
+  {"proton_RESCALED", "F3_RESCALED", "F3_RESCALED", "F3_RESCALED"},
+
   {"proton_RESCALED", "F1_RESCALED", "F2_RESCALED", "F3_RESCALED"},
 };
 
