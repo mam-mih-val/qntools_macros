ce1de7ff850b7484de636589e0d0278923d1a482
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-02-12 14:56:11 +0300

    Seeing if it fixes

--- macro/run8_proton_decompose.cc
+++ macro/run8_proton_decompose.cc
@@ -73,6 +73,7 @@ void run8_proton_decompose(std::string in_file_name, std::string in_calib_file){
       }
 
       for( auto i=size_t{0}; i<qvec.size(); ++i ){
+        auto x1_old = qvec.At(i).x(1) - 
         auto x1_new = qvec.At(i).x(1) * corrM_c[0][0].At(i).Mean() + qvec.At(i).y(1) * corrM_c[1][0].At(i).Mean() + qvec.At(i).x(2) * corrM_c[2][0].At(i).Mean() + qvec.At(i).y(2) * corrM_c[3][0].At(i).Mean();
         auto y1_new = qvec.At(i).x(1) * corrM_c[0][1].At(i).Mean() + qvec.At(i).y(1) * corrM_c[1][1].At(i).Mean() + qvec.At(i).x(2) * corrM_c[2][1].At(i).Mean() + qvec.At(i).y(2) * corrM_c[3][1].At(i).Mean();
         auto x2_new = qvec.At(i).x(1) * corrM_c[0][2].At(i).Mean() + qvec.At(i).y(1) * corrM_c[1][2].At(i).Mean() + qvec.At(i).x(2) * corrM_c[2][2].At(i).Mean() + qvec.At(i).y(2) * corrM_c[3][2].At(i).Mean();
