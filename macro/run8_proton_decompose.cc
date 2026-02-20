190c9e7a4346f82aeecb55754a3d477d1f2476ce
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-02-20 09:38:30 +0300

    Fix

--- macro/run8_proton_decompose.cc
+++ macro/run8_proton_decompose.cc
@@ -115,14 +115,15 @@ void run8_proton_decompose(std::string in_file_name, std::string in_calib_file){
     return [&vec_c, &vec_s, &vec_cov]( Qn::DataContainerQVector qvec, Double_t centrality, Double_t run_id ) -> Qn::DataContainerQVector {
       auto new_qvec = qvec;
       auto c_axis = vec_c[0].GetAxis( "centrality" ); 
-      auto r_axis = vec_c[0].GetAxis( "runId" ); 
       auto c_bin = c_axis.FindBin( centrality );
-      auto r_bin = r_axis.FindBin( run_id );
       auto c_bin_lo = c_axis.GetLowerBinEdge( c_bin );
       auto c_bin_hi = c_axis.GetUpperBinEdge( c_bin );
-      auto r_bin_lo = c_axis.GetLowerBinEdge( r_bin );
-      auto r_bin_hi = c_axis.GetUpperBinEdge( r_bin );
       auto new_c_axis = Qn::AxisD{ "centrality", 1, c_bin_lo, c_bin_hi };
+
+      auto r_axis = vec_c[0].GetAxis( "runId" ); 
+      auto r_bin = r_axis.FindBin( run_id );
+      auto r_bin_lo = r_axis.GetLowerBinEdge( r_bin );
+      auto r_bin_hi = r_axis.GetUpperBinEdge( r_bin );
       auto new_r_axis = Qn::AxisD{ "runId", 1, r_bin_lo, r_bin_hi };
 
       auto vec_c_c = vec_c;
