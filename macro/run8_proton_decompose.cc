ce6c889eab488c601dde037b11d9742e9c30bf20
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-03-10 00:43:48 +0300

    Decomposing with smaller cut on singular values

--- macro/run8_proton_decompose.cc
+++ macro/run8_proton_decompose.cc
@@ -108,7 +108,7 @@ DataContainerMatrix MakeCorrectionMatrix(const vector1d<Qn::DataContainerStatCal
       { s3, s4+s2, c2-c4, s1+s3, c1-c3, s6, 1-c6 },
     };
 
-    auto Minv = RegularizedInverse( M, 5e-2);
+    auto Minv = RegularizedInverse( M, 5e-3 );
     corr_matrix.At(i) = Minv;
   }
   return corr_matrix;
