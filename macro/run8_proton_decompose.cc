f2ee7bc550689e75cdf72258be686dcb9eccfa89
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-03-11 21:13:53 +0300

    attempting to correct up to 3d harmonics

--- macro/run8_proton_decompose.cc
+++ macro/run8_proton_decompose.cc
@@ -12,7 +12,7 @@
 #include <string>
 #include <vector>
 
-constexpr size_t NDIM = 5;
+constexpr size_t NDIM = 7;
 
 using correction_matrix_t = Eigen::Matrix<double, NDIM+1, NDIM+1>;
 using mixing_matrix_t = Eigen::Matrix<double, NDIM-1, NDIM>;
@@ -69,10 +69,12 @@ DataContainerMatrix MakeCorrectionMatrix(const vector1d<Qn::DataContainerStatCal
     auto s6 = vec_s[5].At(i).Mean();
 
     auto M = mixing_matrix_t{ 
-      { c1, 1+c2, s2, c3+c1, s3+s1 },
-      { s1, s2, 1-c2, s3-s1, c1-c3 },
-      { c2, c3+c1, s3-s1, 1+c4, s4 },
-      { s2, s3+s1, c1-c3, s4, 1-c4 },
+      { c1,  1+c2,    s2,   c3+c1,  s3+s1, c2+c4, s4+s2 },
+      { s1,  s2,    1-c2,   s3-s1,  c1-c3, s4-s2, c2-c4 },
+      { c2,  c3+c1, s3-s1,  1+c4,      s4, c5+c1, s5+s1 },
+      { s2,  s3+s1, c1-c3,    s4,    1-c4, s5-s1, c1-c5 },
+      { c3,  c2+c4, s4-s2,  c5+c1,  s5-s1,  1+c6,    s6 },
+      { s3,  s4+s2, c2-c4,  s5+s1,  c1-c5,    s6,  1+-6 },
     };
 
     auto MTM = 2 * M.transpose() * M;
@@ -245,6 +247,9 @@ void run8_proton_decompose(std::string in_file_name, std::string in_calib_file){
     
         auto x2_old = qvec.At(i).x(2);
         auto y2_old = qvec.At(i).y(2);
+
+        auto x3_old = qvec.At(i).x(3);
+        auto y3_old = qvec.At(i).y(3);
     
         auto [Minv, M] = vec_cov.at(c_bin).at(r_bin).At(i);
         
@@ -253,7 +258,7 @@ void run8_proton_decompose(std::string in_file_name, std::string in_calib_file){
           continue;
         }
 
-        auto X1old =  column_t{ x1_old, y1_old, x2_old, y2_old };
+        auto X1old =  column_t{ x1_old, y1_old, x2_old, y2_old, x3_old, y3_old };
         auto b = 2 * M.transpose() * X1old;
         auto b_tilda = Eigen::Matrix<double, NDIM+1, 1>{};
         b_tilda << b, 1;
@@ -264,9 +269,12 @@ void run8_proton_decompose(std::string in_file_name, std::string in_calib_file){
         auto y1_new = static_cast<double>(X1new(2));
         auto x2_new = static_cast<double>(X1new(3));
         auto y2_new = static_cast<double>(X1new(4));
+        auto x3_new = static_cast<double>(X1new(5));
+        auto y3_new = static_cast<double>(X1new(6));
 
         new_qvec.At(i).SetQ( 1, x1_new, y1_new );
         new_qvec.At(i).SetQ( 2, x2_new, y2_new );
+        new_qvec.At(i).SetQ( 3, x3_new, y3_new );
       }
 
       return new_qvec;
