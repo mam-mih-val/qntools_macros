8755fb3dd7ddf33344d3125d9fe1cba174c3e15e
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-07-08 15:01:28 +0300

    debuging is done.

--- macro/proton_v2/run8_mc_proton_correct.cc
+++ macro/proton_v2/run8_mc_proton_correct.cc
@@ -305,7 +305,7 @@ void run8_mc_proton_correct( std::string list,
     .Define( "simProtonY", rapidity_generator(PROTON_M, Y_CM), {"simPz", "simP"} )
     
     .Define( "trIsProton", tr_is_particle, {"trSimIndex", "simIsProton"} )
-    .Range( 1000 )
+    // .Range( 1000 )
 
     .Filter("vtxNtracks > 2")      
   ; 
