f6280abbb276c806e5afd78297e3dcf49e554063
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-08-04 01:09:01 +0300

    Calculating the covariance using the first-order weights

--- macro/proton_v2/mc_calculate_cn_sn.cc
+++ macro/proton_v2/mc_calculate_cn_sn.cc
@@ -147,13 +147,13 @@ void mc_calculate_cn_sn(string inputFiles="qn.root", string outputFile="CnSn.roo
       corrBuilder.AddCorrelationWithInternalReader(fullName, P1::y(h_a), wSumWu1part, wy, corr, corr);
       for( auto h_b = h_a; h_b<=8; h_b++ ){
         fullName = std::string{corrName}.append( ".x" ).append( std::to_string(h_a) ).append( "x" ).append( std::to_string(h_b) );
-        corrBuilder.AddCorrelationWithInternalReader(fullName, P1::xx(h_a, h_b), wSumW2u1part, wy, corr, corr);
+        corrBuilder.AddCorrelationWithInternalReader(fullName, P1::xx(h_a, h_b), wSumWu1part, wy, corr, corr);
         fullName = std::string{corrName}.append( ".x" ).append( std::to_string(h_a) ).append( "y" ).append( std::to_string(h_b) );
-        corrBuilder.AddCorrelationWithInternalReader(fullName, P1::xy(h_a, h_b), wSumW2u1part, wy, corr, corr);
+        corrBuilder.AddCorrelationWithInternalReader(fullName, P1::xy(h_a, h_b), wSumWu1part, wy, corr, corr);
         fullName = std::string{corrName}.append( ".y" ).append( std::to_string(h_a) ).append( "x" ).append( std::to_string(h_b) );
-        corrBuilder.AddCorrelationWithInternalReader(fullName, P1::yx(h_a, h_b), wSumW2u1part, wy, corr, corr);
+        corrBuilder.AddCorrelationWithInternalReader(fullName, P1::yx(h_a, h_b), wSumWu1part, wy, corr, corr);
         fullName = std::string{corrName}.append( ".y" ).append( std::to_string(h_a) ).append( "y" ).append( std::to_string(h_b) );
-        corrBuilder.AddCorrelationWithInternalReader(fullName, P1::yy(h_a, h_b), wSumW2u1part, wy, corr, corr);
+        corrBuilder.AddCorrelationWithInternalReader(fullName, P1::yy(h_a, h_b), wSumWu1part, wy, corr, corr);
       }
     }
   }
