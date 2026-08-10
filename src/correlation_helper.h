8a23f00dda1d3d4bd5b246fb2e29554247ea0939
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-08-10 13:50:10 +0300

    Idk

--- src/correlation_helper.h
+++ src/correlation_helper.h
@@ -129,13 +129,13 @@ auto Define2PartCorrelation( DataFrame& df, Func corr_func, const std::string& f
     component_names[2] = std::string{correlation_name}.append("_x").append(std::to_string(h1)).append("y").append(std::to_string(h2));
     component_names[3] = std::string{correlation_name}.append("_y").append(std::to_string(h1)).append("y").append(std::to_string(h2));
 
-    if constexpr ( std::is_same_v<qvector_t, typename std::decay<Func>::First_t> ){
+    if constexpr ( std::is_same_v<qvector_t, typename Func::First_t> ){
       df = df.Define( component_names[0], [h1, h2]( qvector_t first, qvector_t second ){ return first[h1].x*second[h2].x; }, std::vector{first_name, second_name} );
       df = df.Define( component_names[1], [h1, h2]( qvector_t first, qvector_t second ){ return first[h1].y*second[h2].x; }, std::vector{first_name, second_name} );
       df = df.Define( component_names[2], [h1, h2]( qvector_t first, qvector_t second ){ return first[h1].x*second[h2].y; }, std::vector{first_name, second_name} );
       df = df.Define( component_names[3], [h1, h2]( qvector_t first, qvector_t second ){ return first[h1].y*second[h2].y; }, std::vector{first_name, second_name} );
     } 
-    if constexpr ( std::is_same_v<uvector_t, typename std::decay<Func>::First_t> ) {
+    if constexpr ( std::is_same_v<uvector_t, typename Func::First_t> ) {
       df = df.Define( component_names[0], [h1, h2]( uvector_t first, qvector_t second ){ std::vector<double> res{}; res.reserve( first.size() ); for( auto f : first ){ res.push_back( f[h1].x * second[h2].x ); } return res; }, std::vector{first_name, second_name} );
       df = df.Define( component_names[1], [h1, h2]( uvector_t first, qvector_t second ){ std::vector<double> res{}; res.reserve( first.size() ); for( auto f : first ){ res.push_back( f[h1].y * second[h2].x ); } return res; }, std::vector{first_name, second_name} );
       df = df.Define( component_names[2], [h1, h2]( uvector_t first, qvector_t second ){ std::vector<double> res{}; res.reserve( first.size() ); for( auto f : first ){ res.push_back( f[h1].x * second[h2].y ); } return res; }, std::vector{first_name, second_name} );
