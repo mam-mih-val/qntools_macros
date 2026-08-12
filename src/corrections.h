80c67fd225fc6629c60b8de6b5eef8cf05dc8391
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-08-12 23:13:30 +0300

    Up to 5th harmonic, normalization on sum of squares

--- src/corrections.h
+++ src/corrections.h
@@ -189,7 +189,7 @@ correction_matrix_t PseudoInverse( const correction_matrix_t& M, double l ){
   auto norm = Ur * Ur.transpose();
   auto E = correction_matrix_t{ correction_matrix_t::Zero() };
   for( auto i=size_t{0}; i<norm.cols(); ++i ){
-    E(i, i) = 1.0 / sqrt(norm(i, i));
+    E(i, i) = 1.0 / norm(i, i);
   }
   auto Mpinv = correction_matrix_t{ E * V * Splus * U.transpose() };
   std::cout << "l: " << l << "\nMatrix M:\n" << M << "\nMatrix U:\n" << Ur << "\nS: " << singular_values.transpose() << "\nMatrix S:\n" << Splus << "\nInverse:\n" << Mpinv << "\nE:\n" << E << "\nNorm:\n" << norm;
