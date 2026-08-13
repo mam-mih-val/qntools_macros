232e96215ac2d685536419f94e8f628ce6d88430
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-08-13 14:19:31 +0300

    fix

--- src/corrections.h
+++ src/corrections.h
@@ -199,7 +199,7 @@ correction_matrix_t PseudoInverse( const correction_matrix_t& M, double l ){
   for( auto i=size_t{0}; i<norm.cols(); ++i ){
     E(i, i) = 1.0 / norm(i, i);
   }
-  auto Mpinv = correction_matrix_t{ Ur1 * V * Splus * U.transpose() };
+  auto Mpinv = correction_matrix_t{ Ur1 * Splus * U.transpose() };
   std::cout << "l: " << l << "\nMatrix M:\n" << M << "\nMatrix U:\n" << Ur << "\nS: " << singular_values.transpose() << "\nMatrix S:\n" << Splus << "\nInverse:\n" << Mpinv << "\nE:\n" << Ur1 << "\n\n";
   return Mpinv;
 }
