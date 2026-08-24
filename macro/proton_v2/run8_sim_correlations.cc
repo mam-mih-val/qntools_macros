5a08a2870d296fe7a0d3991f450b3b3f754ecfa4
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-08-24 17:49:32 +0300

    Checking the momentum conservation effect

---
+++ macro/proton_v2/run8_sim_correlations.cc
@@ -0,0 +1,137 @@
+//
+// Created by Misha on 3/7/2023.
+//
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstddef>
+#include <exception>
+#include <limits>
+#include <memory>
+#include <random>
+#include <vector>
+#include <functional>
+
+#include <DataContainer.hpp>
+#include <QnDataFrame.hpp>
+#include "correlation_helper.h"
+#include "bmn_env.h"
+#include "vector_generators.h"
+#include "corrections.h"
+
+void run8_sim_correlations( std::string list ){
+
+  std::cout << "starting execution" << std::endl;
+
+  auto proton_axes = std::vector<Qn::AxisD>{
+    Qn::AxisD{ "centrality", 6, 0, 60 },
+    Qn::AxisD{ "y", 6, 0.0, 1.2 },
+    Qn::AxisD{ "pT", 5, 0.0, 2.0 },
+  };
+  
+  auto qvector_axes = std::vector<Qn::AxisD>{
+    Qn::AxisD{ "centrality", 6, 0, 60 },
+  };
+
+  constexpr size_t NHARM = 2;
+  auto harmonics = std::vector<size_t>(NHARM);
+  std::iota( harmonics.begin(), harmonics.end(), 1 );
+
+  TStopwatch timer;
+  timer.Start();
+  std::string treename = "t";
+  TFileCollection collection( "collection", "", list.c_str() );
+  auto* chain = new TChain( treename.c_str() );
+  chain->AddFileInfoList( collection.GetList() );
+  ROOT::RDataFrame d( *chain );
+  std::cout << "Preparing the RDF" << std::endl;
+  
+  auto dd = GenerateBmnExtendedTree(d, efficiency_histo);
+
+  auto sampled_d = Qn::Correlation::Resample(dd, 100);
+
+  DefineVector( sampled_d, "sim_F1", q_vector< std::vector<float>, std::vector<double> >(harmonics), std::vector<std::string>{"simPhi", "simF1w"} );
+  DefineVector( sampled_d, "sim_F2", q_vector< std::vector<float>, std::vector<double> >(harmonics), std::vector<std::string>{"simPhi", "simF2w"} );
+  DefineVector( sampled_d, "sim_F3", q_vector< std::vector<float>, std::vector<double> >(harmonics), std::vector<std::string>{"simPhi", "simF3w"} );
+  
+  DefineVector(sampled_d, "tru_proton", u_vector< std::vector<float> >( harmonics ), std::vector<std::string>{"simPhi"s} );
+  DefineVector(sampled_d, "psi_rp", psi_rp_vector< double >( harmonics ), std::vector<std::string>{"psiRP"s} );
+  
+  auto p_psi_names = Define2PartCorrelation( sampled_d, CorrFunc2Part< uvector_t, qvector_t >{}, "tru_proton", "psi_rp", std::vector{ std::pair<size_t, size_t>{1, 1} } );
+
+  auto p_f1_names = Define2PartCorrelation( sampled_d, CorrFunc2Part< uvector_t, qvector_t >{}, "tru_proton", "sim_F1", std::vector{ std::pair<size_t, size_t>{1, 1} } );
+  auto p_f2_names = Define2PartCorrelation( sampled_d, CorrFunc2Part< uvector_t, qvector_t >{}, "tru_proton", "sim_F2", std::vector{ std::pair<size_t, size_t>{1, 1} } );
+  auto p_f3_names = Define2PartCorrelation( sampled_d, CorrFunc2Part< uvector_t, qvector_t >{}, "tru_proton", "sim_F3", std::vector{ std::pair<size_t, size_t>{1, 1} } );
+  
+  auto f1_psi_names = Define2PartCorrelation( sampled_d, CorrFunc2Part< qvector_t, qvector_t >{}, "sim_F1", "psi_rp", std::vector{ std::pair<size_t, size_t>{1, 1} } );
+  auto f2_psi_names = Define2PartCorrelation( sampled_d, CorrFunc2Part< qvector_t, qvector_t >{}, "sim_F2", "psi_rp", std::vector{ std::pair<size_t, size_t>{1, 1} } );
+  auto f3_psi_names = Define2PartCorrelation( sampled_d, CorrFunc2Part< qvector_t, qvector_t >{}, "sim_F3", "psi_rp", std::vector{ std::pair<size_t, size_t>{1, 1} } );
+
+  auto p_psi_ptr = std::vector< ROOT::RDF::RResultPtr< Qn::DataContainerStatCollect > >{};
+
+  auto p_f1_ptr = std::vector< ROOT::RDF::RResultPtr< Qn::DataContainerStatCollect > >{};
+  auto p_f2_ptr = std::vector< ROOT::RDF::RResultPtr< Qn::DataContainerStatCollect > >{};
+  auto p_f3_ptr = std::vector< ROOT::RDF::RResultPtr< Qn::DataContainerStatCollect > >{};
+  
+  auto f1_psi_ptr = std::vector< ROOT::RDF::RResultPtr< Qn::DataContainerStatCollect > >{};
+  auto f2_psi_ptr = std::vector< ROOT::RDF::RResultPtr< Qn::DataContainerStatCollect > >{};
+  auto f3_psi_ptr = std::vector< ROOT::RDF::RResultPtr< Qn::DataContainerStatCollect > >{};
+
+  for( const auto& name : p_psi_names ){
+    p_psi_ptr.emplace_back(
+      sampled_d.Book< std::vector<double>, std::vector<int>,  ROOT::VecOps::RVec<ULong64_t>, float, ROOT::VecOps::RVec<float>, ROOT::VecOps::RVec<float> >( CorrelationHelper(proton_axes), {name, "simIsProton", "samples", "centrality", "simProtonY", "simPt" } )
+    ); 
+  }
+
+  for( const auto& name : p_f1_names ){
+    p_f1_ptr.emplace_back(
+      sampled_d.Book< std::vector<double>, std::vector<int>,  ROOT::VecOps::RVec<ULong64_t>, float, ROOT::VecOps::RVec<float>, std::vector<float> >( CorrelationHelper(proton_axes), {name, "simIsProton", "samples", "centrality", "simProtonY", "simPt" } )
+    ); 
+  }
+
+  for( const auto& name : p_f2_names ){
+    p_f2_ptr.emplace_back(
+      sampled_d.Book< std::vector<double>, std::vector<int>,  ROOT::VecOps::RVec<ULong64_t>, float, ROOT::VecOps::RVec<float>, std::vector<float> >( CorrelationHelper(proton_axes), {name, "simIsProton", "samples", "centrality", "simProtonY", "simPt" } )
+    ); 
+  }
+
+  for( const auto& name : p_f3_names ){
+    p_f3_ptr.emplace_back(
+      sampled_d.Book< std::vector<double>, std::vector<int>,  ROOT::VecOps::RVec<ULong64_t>, float, ROOT::VecOps::RVec<float>, std::vector<float> >( CorrelationHelper(proton_axes), {name, "simIsProton", "samples", "centrality", "simProtonY", "simPt" } )
+    ); 
+  }
+
+  for( const auto& name : f1_psi_names ){
+    f1_psi_ptr.emplace_back(
+      sampled_d.Book< double, double,  ROOT::VecOps::RVec<ULong64_t>, float>( CorrelationHelper(qvector_axes, 100, Qn::Stat::WeightType::REFERENCE), {name, "One", "samples", "centrality" } )
+    ); 
+  }
+
+  for( const auto& name : f2_psi_names ){
+    f2_psi_ptr.emplace_back(
+      sampled_d.Book< double, double,  ROOT::VecOps::RVec<ULong64_t>, float>( CorrelationHelper(qvector_axes, 100, Qn::Stat::WeightType::REFERENCE), {name, "One", "samples", "centrality" } )
+    ); 
+  }
+
+  for( const auto& name : f3_psi_names ){
+    f3_psi_ptr.emplace_back(
+      sampled_d.Book< double, double,  ROOT::VecOps::RVec<ULong64_t>, float>( CorrelationHelper(qvector_axes, 100, Qn::Stat::WeightType::REFERENCE), {name, "One", "samples", "centrality" } )
+    ); 
+  }
+
+  auto file_out = std::unique_ptr<TFile, std::function<void(TFile*)> >{ TFile::Open( "corr.root", "RECREATE"), [](auto f){ f->Close(); } };
+  file_out->cd();
+  
+  std::for_each( p_psi_ptr.begin(), p_psi_ptr.end(), [i=0, &p_psi_names]( auto& p ) mutable { p->Write( p_psi_names.at(i).c_str() ); ++i; } );
+  std::for_each( p_f1_ptr.begin(), p_f1_ptr.end(), [i=0, &p_f1_names]( auto& p ) mutable { p->Write( p_f1_names.at(i).c_str() ); ++i; } );
+  std::for_each( p_f2_ptr.begin(), p_f2_ptr.end(), [i=0, &p_f2_names]( auto& p ) mutable { p->Write( p_f2_names.at(i).c_str() ); ++i; } );
+  std::for_each( p_f3_ptr.begin(), p_f3_ptr.end(), [i=0, &p_f3_names]( auto& p ) mutable { p->Write( p_f3_names.at(i).c_str() ); ++i; } );
+  
+  std::for_each( f1_psi_ptr.begin(), f1_psi_ptr.end(), [i=0, &f1_psi_names]( auto& p ) mutable { p->Write( f1_psi_names.at(i).c_str() ); ++i; } );
+  std::for_each( f2_psi_ptr.begin(), f2_psi_ptr.end(), [i=0, &f2_psi_names]( auto& p ) mutable { p->Write( f2_psi_names.at(i).c_str() ); ++i; } );
+  std::for_each( f3_psi_ptr.begin(), f3_psi_ptr.end(), [i=0, &f3_psi_names]( auto& p ) mutable { p->Write( f3_psi_names.at(i).c_str() ); ++i; } );
+
+  auto n_events_filtered = *(dd.Count());
+  std::cout << "Number of filtered events: " << n_events_filtered << std::endl;
+}
