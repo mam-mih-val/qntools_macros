c99c6b6a11672fd31a29d4190c9014eb8c9baff3
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-09-02 14:53:56 +0300

    PCA correction

--- macro/proton_v2/run8_mc_proton_correlations.cc
+++ macro/proton_v2/run8_mc_proton_correlations.cc
@@ -35,7 +35,7 @@ void run8_mc_proton_correlations( std::string list, std::string str_effieciency_
   };
 
   constexpr size_t NHARM = 2;
-  const auto l = double{5e-3};
+  const auto l = double{5e-2};
   auto harmonics = std::vector<size_t>(NHARM);
   std::iota( harmonics.begin(), harmonics.end(), 1 );
 
@@ -102,8 +102,8 @@ void run8_mc_proton_correlations( std::string list, std::string str_effieciency_
   DefineVector(sampled_d, "psi_rp", psi_rp_vector< double >( harmonics ), std::vector<std::string>{"psiRP"s} );
 
   auto calib_file = std::unique_ptr<TFile, std::function<void(TFile*)> >{ TFile::Open( str_calib_file.c_str(), "READ"), [](auto f){ f->Close(); } };
-  auto [vec_p_mean, vec_p_cov] = ReadMeanCov<NHARM*2>("proton", calib_file.get());
-  auto p_correction_container = MakeCorrectionContainer<NHARM>( vec_p_mean, vec_p_cov, TwistRescale<NHARM>{}, l );
+  auto [vec_p_mean, vec_p_cov] = ReadMeanCov<NHARM>("proton", calib_file.get());
+  auto p_correction_container = MakeCorrectionContainer<NHARM>( vec_p_mean, vec_p_cov, PrincipalComponents<NHARM>{}, l );
   auto p_corr_builder = CorrectorBuilder<NHARM>( p_correction_container );
   sampled_d = sampled_d.Define( "proton", p_corr_builder.IssueUVectorCorrector<uvector_t, float, ROOT::VecOps::RVec<float>, ROOT::VecOps::RVec<float> >(), { "ini_proton", "centrality", "trProtonY", "trPt" } );
 
