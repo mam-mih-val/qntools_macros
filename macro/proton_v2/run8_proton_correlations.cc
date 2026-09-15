648e1c1555598540b4c435c650809ca1234c433d
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-09-15 10:59:44 +0300

    Smaller threshold for correction

--- macro/proton_v2/run8_proton_correlations.cc
+++ macro/proton_v2/run8_proton_correlations.cc
@@ -138,7 +138,7 @@ void run8_proton_correlations( std::string file_list,
 
   auto calib_file = std::unique_ptr<TFile, std::function<void(TFile*)> >{ TFile::Open( str_calib_file.c_str(), "READ"), [](auto f){ f->Close(); } };
   auto [vec_p_mean, vec_p_cov] = ReadMeanCov<NHARM>("proton", calib_file.get());
-  auto p_correction_container = MakeCorrectionContainer<NHARM>( vec_p_mean, vec_p_cov, PrincipalComponents<NHARM>{}, 0.1 );
+  auto p_correction_container = MakeCorrectionContainer<NHARM>( vec_p_mean, vec_p_cov, PrincipalComponents<NHARM>{}, 5e-3 );
   auto p_corr_builder = CorrectorBuilder<NHARM>( p_correction_container );
   sampled_d = sampled_d.Define( "proton", p_corr_builder.IssueUVectorCorrector<uvector_t, float, UInt_t, ROOT::VecOps::RVec<float>, ROOT::VecOps::RVec<float> >(), { "ini_proton", "centrality", "runId", "trProtonY", "trPt" } );
 
