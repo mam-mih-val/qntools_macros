7b2d1d253b062e562b901b247514a03ec5c19d6b
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-09-24 21:40:38 +0300

    Up to 16th harmonic

--- macro/proton_v2/run8_proton_fill.cc
+++ macro/proton_v2/run8_proton_fill.cc
@@ -33,8 +33,8 @@ void run8_proton_fill(std::string file_list,
     Qn::AxisD{ "runId", 4, 7100, 8300 },
     // Qn::AxisD{ "y", 6, 0.0, 1.2 },
     // Qn::AxisD{ "pT", 5, 0.0, 2.0 },
-    Qn::AxisD{ "y", std::vector<double>{ 0, 0.2, 0.4, 0.5, 0.6, 0.7, 0.75, 0.85, 0.9, 1, 1.1, 1.2 } },
-    Qn::AxisD{ "pT", std::vector<double>{ 0, 0.25, 0.3, 0.4, 0.45, 0.55, 0.6, 0.7, 0.8, 1.05, 2.0 } },
+    Qn::AxisD{ "y", std::vector<double>{ 0, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2 } },
+    Qn::AxisD{ "pT", std::vector<double>{ 0, 0.2, 0.4, 0.5, 0.6, 0.8, 1.0, 1.5, 2.0 } },
   };
 
   auto qvector_axes = std::vector<Qn::AxisD>{
@@ -113,7 +113,7 @@ void run8_proton_fill(std::string file_list,
   std::for_each( f3_mod.begin(), f3_mod.end(), [](auto& m){ m += 1; } );
   std::for_each( f4_mod.begin(), f4_mod.end(), [](auto& m){ m += 1; } );
 
-  auto harmonics = std::vector<size_t>( 8 );
+  auto harmonics = std::vector<size_t>( 4 );
   std::iota( harmonics.begin(), harmonics.end(), 1 );
 
   auto qvector_harmonics = std::vector<size_t>( 2 );
